d921b7613dc0a76df08eee0669b05397d150a50e
mch3000 <strange.q@gmail.com>
2020-06-11 00:45:58 +1000

    cleaning up code a bit

--- ladder_system.py
+++ ladder_system.py
@@ -1,19 +1,67 @@
 #!/usr/bin/python3
 
+# Terms:
+#
+# 'teams' - referred to by a positive integer (e.g. team 2) - objects that need to be matched up
+# 'match' - grouping two or more teams together into a 'match', which will have a 'score'
+# 'score' - number given to the match, based on 'costs', where higher is worse
+# 'round' - collection of matches where all teams are matched up - has a total score
+
+
 from timeit import default_timer as timer
 import sys
 import copy
 
+# Find the best matchups, given teams, cost matrix, and match_size
+# This function to prepares and calls the generator all_matchups
+
+def best_matchups(teams,costs,match_size):
+    starttime = timer()
+    
+    global lowest_score, recursion_level
+    
+    lowest_score = 99999.0
+    recursion_level = -1
+    best_match = None
+
+    # Create the generator function, that will be called later:
+    match_gen = all_matchups_checks(teams,costs,match_size)
+    
+    ct = 0
+    ctn = 0
+    
+    print ('Calling generator')
+    
+    for match in match_gen:
+        #print ('TEST',current_state,'Best so far?',match!=None)
+        
+        if match != None:
+            # this match should be the new best (lowest) score:
+            ct+=1
+            score = 0.0
+            for gme in match:
+                score+=gme[0]
+            if score<lowest_score:
+                lowest_score = score
+                best_match = copy.deepcopy(match)
+        else:
+            # this match was not better than current best
+            ctn+=1
+        
+        # Print status updates every 1,000,000 matches:
+        if ctn%1000000==0:
+            print ("<p>",'STATUS:',ct,ctn,lowest_score,round(timer()-starttime,3),'s',"</p>")
+            #print ("<p>",'STATUS:',ct,ctn,lowest_score,"</p>")
+
+    print ('Time taken: ',round(timer()-starttime,4))
+    
+    #for r in best_match:
+    #    print (r)
+        
+    return best_match
 
 
-def calc_cost(current_match_t,new_team,costs):
-    cost = 0.0
-    # for each team already in match:
-    for ts in current_match_t:
-        cost += costs[ts][new_team]
-    return cost
-
-
+# Check inputs before calling all_matchups
 def all_matchups_checks(teams,costs,match_size=2):
     if match_size < 2:
         print('Match size must be 2 or more')
@@ -31,19 +79,21 @@ def all_matchups_checks(teams,costs,match_size=2):
     return all_matchups(teams,costs,match_size)
 
 
-# Function that searches for set of pairings with lowest total cost
+# all_matchups
+# Function that searches for matchups with lowest total cost
 # ** NOTE ** this is recursive!
-# ** NOTE ** the 'yield' command is tricky - later calls to this function will return to this spot!
+# ** NOTE ** the 'yield' command is complicated - later calls to this function will return to this spot!
+#
 # Inputs:
 # teams - list of teams that haven't been matched yet - initially len n, but gets shorter
-# costs - list of len n of list of len n (n x n marix of costs)
+# costs - list of n lists of n floats (n x n marix of costs)
 # match_size - how many teams play in each match. default is 2 - pairs.
 # score - total score so far
 # current_match - passing current match details on for lower recursions
 #
 # Global variables:
-# state_level - the "level" of the tree search. top level is 0, should go to n/2
-# current_state - NOT USED
+# recursion_level - the "level" of the tree search. top level is 0, should go to n/2
+# lowest_score - the lowest score found so far - used to stop searches down branches if score is already above this amount
 #
 # Function variables:
 # starting_index - to account for the first team being added to the match
@@ -52,19 +102,21 @@ def all_matchups_checks(teams,costs,match_size=2):
 
 
 def all_matchups(teams,costs,match_size=2,score=0.0,current_match=[0.0]):
-    global state_level,current_state
+    
+    global recursion_level
+    # not sure why lowest_score doesn't need to be declared too - maybe because it's not changed here?
     
     # go 'down' the tree search one level:
-    state_level += 1
-    ws = ' '*state_level*4
+    recursion_level += 1
     
-    #print(ws,'Calling all_matchups, new state_level: ',state_level)
+    #ws = ' '*recursion_level*4
+    #print(ws,'Calling all_matchups, new recursion_level: ',recursion_level)
     #print(ws,'Teams left: ',len(teams))
 
     # if there aren't any teams left, go back up
     if len(teams) == 0:
         yield teams
-        state_level += -1
+        recursion_level += -1
         return
     
     # if no current match, start with first team in list:
@@ -73,157 +125,62 @@ def all_matchups(teams,costs,match_size=2,score=0.0,current_match=[0.0]):
         starting_index = 1
     else:
         starting_index = 0
-        #skip over the teams already tested:
-        #starting_index = current_match[1]-
         
-    #print(ws,'Current match: ',current_match)
-    
     # for every other team in team list:
     for i in range(starting_index,len(teams)):
         if (teams[i]>current_match[1][-1]):
             # cost of current adding team to current match
             new_score = 0.0
-            # for each team already in match:
+            # cost vs each team already in match:
             for ts in current_match[1]:
                 new_score += costs[ts][teams[i]]
-            
-            #new_score = calc_cost(current_match[1],teams[i],costs)
-            
-            #print(ws,'Cost to add team',teams[i],' to match:',new_score,current_match)
-            #print(ws,'score',score)
-            # if total cost so far is less than global_max, keep going (otherwise no point in continuing)
-            # global_max is updated in parent function when generator is called
-            if new_score+score<global_max: # and s<50.0:
+
+            # only keep going if total score so far is still less than lowest_score, (otherwise skip - no point in continuing)
+            # lowest_score is updated in parent function when generator is called
+            if new_score+score<lowest_score:
                 # update score2 with total score so far
                 score2 = new_score+score
                 
-                # add new team to the match
+                # add the team to the match
                 current_match[0] += new_score
                 current_match[1].append(teams[i])
-                
-                #print(ws,'Accepted. Total score now: ',score2)
-                #print(ws,'Current match now: ',current_match)
                 
                 #if current match is incomplete:
                 if len(current_match[1]) < match_size:
                     # continue down the tree with remaining teams
-                    for rest in all_matchups(teams[starting_index:i]+teams[i+1:],costs,match_size,score2,current_match):
-                        # if no teams left to try, go back up
-                        if rest==None:
-                            #print(ws,'No teams left to try or global max reached.')
-                            yield None
-                        
-                        # else, combine pairs from lower down, and go back up
-                        else:
-                            #print(ws,'Passing only this up: ',rest)
-                            yield rest
+                    for lower_results in all_matchups(teams[starting_index:i]+teams[i+1:],costs,match_size,score2,current_match):
+                        # pass results back on up
+                        yield lower_results
                 else:
                     # continue down with new match:
-                    #print(ws,'New match')
-                    for rest in all_matchups(teams[starting_index:i]+teams[i+1:],costs,match_size,score2,[0.0]):
-                        # if no teams left to try, go back up
-                        if rest==None:
-                            #print(ws,'No teams left to try or global max reached.')
+                    for lower_results in all_matchups(teams[starting_index:i]+teams[i+1:],costs,match_size,score2,[0.0]):
+                        # returns None if no teams left, or score worse than lowest_score
+                        if lower_results==None:
                             yield None
                         
-                        # else, combine pairs from lower down, and go back up
+                        # else, combine results from lower down, and go back up
                         else:
-                            #print(ws,'Passing this up: ',rest)
-                            yield [current_match] + rest
+                            yield [current_match] + lower_results
                             
-                # remove team from current_match?
+                # remove team from current_match
                 current_match[0] += -new_score
                 current_match[1].pop()
                 
-            # else stop searching this branch and go back up
+            # else score more than lowest_score, so stop searching this branch and go back up
             else:
-                #print(ws,'Going back up, total score too high:',new_score+score )
                 yield None
                 
-            #print 'SL',state_level,current_state,len(teams)
-           
-    # set current_state ??????
-    current_state[state_level]=1
     
-    # going back up a level so reduce state_level by 1
-    state_level += -1
-    #print(ws,'End of function - going back up to state_level: ',state_level)
-
-
-# preparing and calling generator
-
-def best_matchups(teams,costs,reset_state,best_cost,best_state,match_size):
-    # badness must be list of lists
-    starttime = timer()
-    
-    global global_max, current_state, state_level
-    global_max = best_cost+1.0
-    best_score = global_max
-    state_level = -1
-    current_state = [1 for i in range(0,len(teams))]
-    best_match = None
-    if reset_state!=None:
-        current_state = reset_state
-        if best_state!=None:
-            best_match = best_state
-        else:
-            best_match = current_state
-
-    # Create the generator function, that will be called later:
-    match_gen = all_matchups_checks(teams,costs,match_size)
-    
-    ct = 0
-    ctn = 0
-    better_found = False
-    
-    print ('Calling pair generator')
-    
-    for match in match_gen:
-        #print ('TEST',current_state,'Best so far?',match!=None)
-        if match != None:
-            #print ('TEST - best so far:')
-            ct+=1
-            score = 0.0
-            for gme in match:
-                #print (gme)
-                score+=gme[0]
-            if score<best_score:
-                best_score = score
-                best_match = copy.deepcopy(match)
-                best_state = [c for c in current_state]
-                global_max = best_score
-                better_found = True
-        else:
-            ctn+=1
-        
-        # ???? not sure why this is here
-        if ctn%1000000==0:
-            if ctn>0 and ctn%1000000==0:
-                print ("<p>",'STATUS:',ct,ctn,best_score,round(timer()-starttime,3),'s',"</p>")
-                #print ("<p>",'STATUS:',ct,ctn,best_score,"</p>")
-        
-        # this bit was to exit if it took too long to calculate? more than 240.0 seconds?
-        
-        #if ctn%10000==0 and round(time.time()-starttime)>240.0:
-        #    print ("<p>",'STATUS:',ct,ctn,best_score,round(time.time()-starttime,1),'s',"</p>")
-        #    print ("<p>",'STATUS:',best_match,'s',"</p>")
-        #    print ("<p>",'TAKING TOO LONG - JSON-EXITING',"</p>")
-        #    return [best_state,current_state]
-        #    break
-
-    print ('Time taken: ',round(timer()-starttime,4))
-    
-    #for r in best_match:
-    #    print (r)
-        
-    return best_match
+    # going back up a level so reduce recursion_level by 1
+    recursion_level += -1
 
 
 if __name__ == '__main__':
+    
     # Testing:
     import random
     
-    teams = [i for i in range(18)]
+    teams = [i for i in range(15)]
     
     random.seed(2)
     
@@ -239,19 +196,16 @@ if __name__ == '__main__':
             else:
                 costs[i].append(99.0)
 
-    best_state = None
-    current_state = None
-    best_cost = 99999.0
-    match_size = 2
+    match_size = 3
 
     print ('DEBUG ')
     print (teams)
     for i in range(len(costs)):
         print (costs[i])
 
-    results = best_matchups(teams,costs,current_state,best_cost,best_state,match_size)
+    results = best_matchups(teams,costs,match_size)
     
-    print ('AFTER RUNNING ')
+    print ('RESULTS ')
     for r in results:
         print (r)
 
